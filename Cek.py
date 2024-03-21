6cf6b7349f9b2f110e34f756f97152f75ba094d3
Sanghitam101 <164131140+Sanghitam101@users.noreply.github.com>
2024-03-21 10:20:32 +0800

    Hek.py

--- Cek.py
+++ Cek.py
@@ -6,12 +6,7 @@ def slowprint(s):
                 sys.stdout.write(c)
                 sys.stdout.flush()
                 time.sleep(1./50)import sys
-import time
-def slowprint(s):
-        for c in s + '\n':
-                sys.stdout.write(c)
-                sys.stdout.flush()
-                time.sleep(1./50)
+
 
 slowprint ("""
 hallo word""")
